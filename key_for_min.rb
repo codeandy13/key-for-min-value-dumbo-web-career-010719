07624c33a1250603174bb242ec956b242a510f33
Andrew Kim <akim618@gmail.com>
2018-12-28 23:44:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+small < 1000
 def key_for_min_value(name_hash)
+  
+  name_hash.each do |key, value|
+  if key < small
+    small = key
+  smallest_name = key  
 
+  end
+end
+  smallest_name
 end
