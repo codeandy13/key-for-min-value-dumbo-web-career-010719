afca4f041138f5686de171a27f7213e80da67391
Andrew Kim <akim618@gmail.com>
2018-12-28 23:49:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_name = nill
   small = 1000
   name_hash.each do |key, value|
-  if value < small
+  if value <= small
     small = value
     smallest_name = key
 
