2d08118d4db8e52f5f602c9b22df169a3527bd8f
Andrew Kim <akim618@gmail.com>
2018-12-28 23:44:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 small < 1000
 def key_for_min_value(name_hash)
-  
+
   name_hash.each do |key, value|
   if key < small
     small = key
-  smallest_name = key  
+  smallest_name = key
 
   end
 end
